152875070f4882fe8ed748141d883130b0a809e2
bogatovnikita <bogatovnikita@gmail.com>
2021-09-15 20:53:44 +0300

    Init commit

--- app/src/main/java/com/bogatovnikita/mycalculator/MainActivity.java
+++ app/src/main/java/com/bogatovnikita/mycalculator/MainActivity.java
@@ -3,13 +3,15 @@ package com.bogatovnikita.mycalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView entryFieldTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 }
