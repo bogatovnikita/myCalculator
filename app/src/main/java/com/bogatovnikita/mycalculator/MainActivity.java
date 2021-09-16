e53a8c1c837b8d9694f798f8d62f387906623d4f
bogatovnikita <bogatovnikita@gmail.com>
2021-09-16 19:46:13 +0300

    init commit

--- app/src/main/java/com/bogatovnikita/mycalculator/MainActivity.java
+++ app/src/main/java/com/bogatovnikita/mycalculator/MainActivity.java
@@ -3,8 +3,6 @@ package com.bogatovnikita.mycalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
