85ee7fd94f1482bb1835470a3a2493dad73469c5
bogatovnikita <bogatovnikita@gmail.com>
2021-09-20 20:12:25 +0300

    added a separate class for program logic

--- app/src/main/java/com/bogatovnikita/mycalculator/MainActivity.java
+++ app/src/main/java/com/bogatovnikita/mycalculator/MainActivity.java
@@ -2,17 +2,12 @@ package com.bogatovnikita.mycalculator;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    String stringInputTextView = "";
-    String symbolOperation = "";
-    double valueFirst;
-    double valueSecond;
-    double temp;
+    Buttons buttons = new Buttons();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,17 +18,16 @@ public class MainActivity extends AppCompatActivity {
         if (savedInstanceState != null && savedInstanceState.containsKey("stringInputTextView_key")
                 && savedInstanceState.containsKey("symbolOperation_key") && savedInstanceState.containsKey("valueFirst_key")
                 && savedInstanceState.containsKey("valueSecond_key") && savedInstanceState.containsKey("temp_key")) {
-            stringInputTextView = savedInstanceState.getString("stringInputTextView_key");
-            symbolOperation = savedInstanceState.getString("symbolOperation_key");
-            valueFirst = savedInstanceState.getDouble("valueFirst_key");
-            valueSecond = savedInstanceState.getDouble("valueSecond_key");
-            temp = savedInstanceState.getDouble("temp_key");
+            buttons.stringInputTextView = savedInstanceState.getString("stringInputTextView_key");
+            buttons.symbolOperation = savedInstanceState.getString("symbolOperation_key");
+            buttons.valueFirst = savedInstanceState.getDouble("valueFirst_key");
+            buttons.valueSecond = savedInstanceState.getDouble("valueSecond_key");
+            buttons.temp = savedInstanceState.getDouble("temp_key");
         }
-        inputTextView.getText();
 
         Button deleteAllValue = findViewById(R.id.delete_all_button);
         deleteAllValue.setOnClickListener(view -> {
-            clearAll();
+            buttons.clearAllCount();
             inputTextView.setText("");
         });
 
@@ -49,202 +43,117 @@ public class MainActivity extends AppCompatActivity {
 
         Button divide = findViewById(R.id.divide_button);
         divide.setOnClickListener(view -> {
-            if (symbolOperation.isEmpty() && !stringInputTextView.isEmpty()) {
-                symbolOperation = "/";
-                valueFirst = Double.parseDouble(stringInputTextView);
-                clear();
-                inputTextView.setText(stringInputTextView);
+            if (buttons.divideButton()) {
+                buttons.clearTextView();
+                inputTextView.setText(buttons.stringInputTextView);
             }
         });
 
         Button seven = findViewById(R.id.seven_button);
         seven.setOnClickListener(view -> {
-            if (stringInputTextView.length() <= 7)
-                stringInputTextView = stringInputTextView.concat("7");
-            inputTextView.setText(stringInputTextView);
-            if (!symbolOperation.isEmpty() && stringInputTextView.isEmpty()) {
-                stringInputTextView = stringInputTextView.concat("7");
-                inputTextView.setText(stringInputTextView);
-            }
+            if (buttons.buttonSeven())
+                inputTextView.setText(buttons.stringInputTextView);
         });
 
         Button eight = findViewById(R.id.eight_button);
         eight.setOnClickListener(view -> {
-            if (stringInputTextView.length() <= 7)
-                stringInputTextView = stringInputTextView.concat("8");
-            inputTextView.setText(stringInputTextView);
-            if (!symbolOperation.isEmpty() && stringInputTextView.isEmpty()) {
-                stringInputTextView = stringInputTextView.concat("8");
-                inputTextView.setText(stringInputTextView);
-            }
+            if (buttons.buttonEight())
+                inputTextView.setText(buttons.stringInputTextView);
         });
 
         Button nine = findViewById(R.id.nine_button);
         nine.setOnClickListener(view -> {
-            if (stringInputTextView.length() <= 7)
-                stringInputTextView = stringInputTextView.concat("9");
-            inputTextView.setText(stringInputTextView);
-            if (!symbolOperation.isEmpty() && stringInputTextView.isEmpty()) {
-                stringInputTextView = stringInputTextView.concat("9");
-                inputTextView.setText(stringInputTextView);
-            }
+            if (buttons.buttonNine())
+                inputTextView.setText(buttons.stringInputTextView);
         });
 
         Button multiply = findViewById(R.id.multiply_button);
         multiply.setOnClickListener(view -> {
-            if (symbolOperation.isEmpty() && !stringInputTextView.isEmpty()) {
-                symbolOperation = "*";
-                valueFirst = Double.parseDouble(stringInputTextView);
-                clear();
-                inputTextView.setText(stringInputTextView);
+            if (buttons.multiplyButton()) {
+                buttons.clearTextView();
+                inputTextView.setText(buttons.stringInputTextView);
             }
         });
 
         Button four = findViewById(R.id.four_button);
         four.setOnClickListener(view -> {
-            if (stringInputTextView.length() <= 7)
-                stringInputTextView = stringInputTextView.concat("4");
-            inputTextView.setText(stringInputTextView);
-            if (!symbolOperation.isEmpty() && stringInputTextView.isEmpty()) {
-                stringInputTextView = stringInputTextView.concat("4");
-                inputTextView.setText(stringInputTextView);
-            }
+            if (buttons.buttonFour())
+                inputTextView.setText(buttons.stringInputTextView);
         });
 
         Button five = findViewById(R.id.five_button);
         five.setOnClickListener(view -> {
-            if (stringInputTextView.length() <= 7)
-                stringInputTextView = stringInputTextView.concat("5");
-            inputTextView.setText(stringInputTextView);
-            if (!symbolOperation.isEmpty() && stringInputTextView.isEmpty()) {
-                stringInputTextView = stringInputTextView.concat("5");
-                inputTextView.setText(stringInputTextView);
-            }
+            if (buttons.buttonFive())
+                inputTextView.setText(buttons.stringInputTextView);
         });
 
         Button six = findViewById(R.id.six_button);
         six.setOnClickListener(view -> {
-            if (stringInputTextView.length() <= 7)
-                stringInputTextView = stringInputTextView.concat("6");
-            inputTextView.setText(stringInputTextView);
-            if (!symbolOperation.isEmpty() && stringInputTextView.isEmpty()) {
-                stringInputTextView = stringInputTextView.concat("6");
-                inputTextView.setText(stringInputTextView);
-            }
+            if (buttons.buttonSix())
+                inputTextView.setText(buttons.stringInputTextView);
         });
 
         Button subtract = findViewById(R.id.subtract_button);
         subtract.setOnClickListener(view -> {
-            if (symbolOperation.isEmpty() && !stringInputTextView.isEmpty()) {
-                symbolOperation = "-";
-                valueFirst = Double.parseDouble(stringInputTextView);
-                clear();
-                inputTextView.setText(stringInputTextView);
+            if (buttons.subtractButton()) {
+                buttons.clearTextView();
+                inputTextView.setText(buttons.stringInputTextView);
             }
         });
 
         Button three = findViewById(R.id.three_button);
         three.setOnClickListener(view -> {
-            if (stringInputTextView.length() <= 7)
-                stringInputTextView = stringInputTextView.concat("3");
-            inputTextView.setText(stringInputTextView);
-            if (!symbolOperation.isEmpty() && stringInputTextView.isEmpty()) {
-                stringInputTextView = stringInputTextView.concat("3");
-                inputTextView.setText(stringInputTextView);
-            }
+            if (buttons.buttonThree())
+                inputTextView.setText(buttons.stringInputTextView);
         });
 
         Button two = findViewById(R.id.two_button);
         two.setOnClickListener(view -> {
-            if (stringInputTextView.length() <= 7)
-                stringInputTextView = stringInputTextView.concat("2");
-            inputTextView.setText(stringInputTextView);
-            if (!symbolOperation.isEmpty() && stringInputTextView.isEmpty()) {
-                stringInputTextView = stringInputTextView.concat("2");
-                inputTextView.setText(stringInputTextView);
-            }
+            if (buttons.buttonTwo())
+                inputTextView.setText(buttons.stringInputTextView);
         });
 
         Button one = findViewById(R.id.one_button);
         one.setOnClickListener(view -> {
-            if (stringInputTextView.length() <= 7)
-                stringInputTextView = stringInputTextView.concat("1");
-            inputTextView.setText(stringInputTextView);
-            if (!symbolOperation.isEmpty() && stringInputTextView.isEmpty()) {
-                stringInputTextView = stringInputTextView.concat("1");
-                inputTextView.setText(stringInputTextView);
-            }
+            if (buttons.buttonOne())
+                inputTextView.setText(buttons.stringInputTextView);
         });
 
         Button fold = findViewById(R.id.fold_button);
         fold.setOnClickListener(view -> {
-            if (symbolOperation.isEmpty() && !stringInputTextView.isEmpty()) {
-                symbolOperation = "+";
-                valueFirst = Double.parseDouble(stringInputTextView);
-                clear();
-                inputTextView.setText(stringInputTextView);
+            if (buttons.foldButton()) {
+                buttons.clearTextView();
+                inputTextView.setText(buttons.stringInputTextView);
             }
         });
 
         Button nullB = findViewById(R.id.null_button);
         nullB.setOnClickListener(view -> {
-            if (stringInputTextView.length() <= 7)
-                stringInputTextView = stringInputTextView.concat("0");
-            inputTextView.setText(stringInputTextView);
+            if (buttons.buttonNull())
+                inputTextView.setText(buttons.stringInputTextView);
         });
 
         Button comma = findViewById(R.id.comma_button);
         comma.setOnClickListener(view -> {
-            if (!stringInputTextView.isEmpty() && stringInputTextView.length() <= 7) {
-                stringInputTextView = stringInputTextView.concat(".");
-                inputTextView.setText(stringInputTextView);
-            }
+            if (buttons.buttonComma())
+                inputTextView.setText(buttons.stringInputTextView);
         });
 
         Button equals = findViewById(R.id.equals_button);
         equals.setOnClickListener(view -> {
-            equalsCount();
-            inputTextView.setText(Double.toString(temp));
-            clearAll();
-
+            buttons.equalsCount();
+            inputTextView.setText(Double.toString(buttons.temp));
+            buttons.clearAllCount();
         });
-    }
-
-    public void clear() {
-        stringInputTextView = "";
-    }
-
-    public void clearAll() {
-        stringInputTextView = "";
-        symbolOperation = "";
-        double valueFirst = 0.0f;
-        double valueSecond = 0.0f;
-        double temp = 0.0f;
-    }
-
-    public void equalsCount() {
-        valueSecond = Double.parseDouble(stringInputTextView);
-        if (symbolOperation.equals("/")) {
-            temp = valueFirst / valueSecond;
-        }
-        if (symbolOperation.equals("*")) {
-            temp = valueFirst * valueSecond;
-        }
-        if (symbolOperation.equals("-")) {
-            temp = valueFirst - valueSecond;
-        }
-        if (symbolOperation.equals("+")) {
-            temp = valueFirst + valueSecond;
-        }
     }
 
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putString("stringInputTextView_key", stringInputTextView);
-        outState.putString("symbolOperation_key", symbolOperation);
-        outState.putDouble("valueFirst_key", valueFirst);
-        outState.putDouble("valueSecond_key", valueSecond);
-        outState.putDouble("temp_key", temp);
+        outState.putString("stringInputTextView_key", buttons.stringInputTextView);
+        outState.putString("symbolOperation_key", buttons.symbolOperation);
+        outState.putDouble("valueFirst_key", buttons.valueFirst);
+        outState.putDouble("valueSecond_key", buttons.valueSecond);
+        outState.putDouble("temp_key", buttons.temp);
     }
 }
